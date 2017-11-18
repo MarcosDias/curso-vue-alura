da96f098adf47376705226c6354162c8f4a34acf
Marcos Dias <marcos.a.melo.dias@gmail.com>
2017-11-18 18:54:20 -0200

    Reorganizacao do projeto. Adição do back-end no projeto

---
+++ alurapic/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      titulo: 'Alurapic',
+      fotos: [{
+        url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+        titulo: 'Cachorro'
+      }, {
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM',
+        titulo: 'Gato'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
